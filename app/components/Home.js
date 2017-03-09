73bd203133cdb999838803bf7eaa846caacb1912
rakin <rafser@odduu.com>
2017-03-09 16:33:03 +0600

    Rakin Afser

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,20 +2,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.css';
-
+import connectOdduu from '../../app/service/connect_odduu'
 
 export default class Home extends Component {
   getStore() {
     return this.props.primaryData || [];
   }
   render() {
+    connectOdduu(this.getStore())
     return (
       <div>
         <div className={styles.container}>
           <h2>Installing....</h2>
             {this.getStore().map(function(t){
                 return <div > {t[0]} </div>
-            })}
+            })
+
+          }
         </div>
       </div>
     );
--- app/service/connect_odduu.js
+++ app/service/connect_odduu.js
@@ -1,5 +1,6 @@
 var Promise=require('bluebird')
 var sh = Promise.promisifyAll(require('shelljs'));
+var fs=Promise.promisifyAll(require('fs-extra'));
 //import {store} from '../index'
 let cmds=[
   'mkdir -p /mnt/var/cache/pacman',
@@ -8,12 +9,34 @@ let cmds=[
   'mkdir -p /mnt/var/cache/pacman/pkg',
   'mkdir -p /root/.ssh'
   ]
-export default function(){
+ let cmds1=[
+        'cp  id_rsa* authorized_keys /root/.ssh'
+  ]  
+export default function(store){
   console.log('Connecting Odduu Server ...')
   return beforeIDrsa().then((res)=>{
-    //echo store.getState().id_rsa.pub > .ssh/id_rsa.pub & id_rsa & authorised_keys
-    console.log('in connect_odduu')
-    //next step
+    new Promise((resolve,reject)=>{
+      store.map((single)=>{
+      if(single[0].includes('BEGIN RSA PRIVATE KEY') ===true){
+        console.log('echo '+single[0]+' > id_rsa')
+        fs.writeFileAsync('./id_rsa',single[0]).then((success)=>{
+          console.log('Successfully completed generating id_rsa')
+        })
+      }
+      if(single[0].includes('ssh-rsa') ===true){
+        fs.writeFileAsync('./id_rsa.pub',single[0]).then((success)=>{
+          console.log('Successfully completed generating id_rsa.pub')
+        })
+      }
+      resolve('done')
+    })
+    }).then((success)=>{
+      console.log('Generating id_rsa, id_rsa.pub ',success);
+
+    })
+   
+   
+    
   })
 
 }
@@ -23,6 +46,7 @@ function beforeIDrsa(){
   return Promise.all(cmds.map((cmd)=>{
     
     return sh.execAsync(cmd).then(item=>{
+
       return Promise.resolve('Completed --- '+item)
     })
   
